4e906bc6597bd9f24c3ff9478a3e0d203c673197
Oleg4000 <olegklim568@gmail.com>
2023-02-19 21:01:58 +0200

    function

---
+++ Lesson 17/jssc.js
@@ -0,0 +1,34 @@
+"use strict";
+let num = 20;
+
+function showFirstMessage(text) {
+  console.log(text);
+  let num = 10;
+  console.log(num);
+}
+
+showFirstMessage("Hello Oleh");
+console.log(num);
+
+function calc(a, b) {
+  return a + b;
+}
+console.log(calc(4, 3));
+console.log(calc(5, 3));
+console.log(calc(4, 7));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+//function expressin
+const logger = function () {
+  console.log("Hello Oleh");
+};
+
+logger();
+
+//стрілочна ф-ція
+const calc = (a, b) => a + b;
