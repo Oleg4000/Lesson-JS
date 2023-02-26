7fd01c309aae19418006ff2dad4572df9a9c0557
Oleg4000 <olegklim568@gmail.com>
2023-02-26 18:55:02 +0200

    methods

---
+++ Lesson 18/sc.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const str = "Test";
+
+console.log(str.toLowerCase());
+//console.log(str.toLocaleUpperCase());
+//console.log((str[2] = "d"));
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Helllo world";
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
